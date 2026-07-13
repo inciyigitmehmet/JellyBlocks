a47bdffd863ae45b1f3c02cb0451547d58bf8668
inciyigitmehmet <inciyigitmehmet@gmail.com>
2026-07-13 10:55:01 +0300

    first commit

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public int width = 5;
+    public int height = 5;
+    // grid'in alanı
+    public GameObject tilePrefab;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+
+    {
+
+    }
+    void GenerateGrid()
+
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+
+
+            }
+
+
+        }
+
+
+    }
+}
